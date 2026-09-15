d3bfed82a206be89f5c75ddde6abb9911b5f389c
rebeccaschou <rstchou@gmail.com>
2026-09-15 10:56:20 -0400

    fix: remove unnecessary dependencies from app.ts

--- ui/src/app/app.ts
+++ ui/src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationBar } from './layout/navigation-bar/navigation-bar';
-import { AccountOverview } from './dashboard/account-overview/account-overview';
-import { AccountDetails } from './dashboard/account-details/account-details';
-import { AccountSelect } from './dashboard/account-select/account-select';
 
 @Component({
-  imports: [RouterOutlet, NavigationBar, AccountOverview, AccountDetails, AccountSelect],
+  imports: [RouterOutlet, NavigationBar],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
