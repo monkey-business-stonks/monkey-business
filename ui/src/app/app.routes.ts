f2a9c7dbc1cbf9bfeef4ad6bda43c8a747ff5f29
rebeccaschou <rstchou@gmail.com>
2026-09-15 11:19:26 -0400

    feat: basic navigation between pages

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard/dashboard';
+import { Trade } from './trade/trade/trade';
+import { History } from './history/history/history';
 
 export const routes: Routes = [
-  { path: '', component: Dashboard },
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'trade', component: Trade },
+  { path: 'history', component: History },
 ];
