4b390fa752ee3f9c7eb06b24225b973831d555a0
rebeccaschou <rstchou@gmail.com>
2026-09-15 13:01:29 -0400

    chore: refactor account details table to reusable component

--- ui/src/app/dashboard/account-details/account-details.ts
+++ ui/src/app/dashboard/account-details/account-details.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Table, TableColumn } from '../../shared/table/table';
 
 @Component({
-  imports: [],
+  imports: [Table, CommonModule],
   selector: 'account-details',
   styleUrl: './account-details.css',
   templateUrl: './account-details.html',
 })
-export class AccountDetails {}
+export class AccountDetails {
+  columns: TableColumn[] = [
+    { key: 'ticker', header: 'Asset / Ticker', align: 'left' },
+    { key: 'quantity', header: 'Quantity', isMonospace: true, paddingX: 'var(--space-xl)' },
+    { key: 'avgCost', header: 'Avg Cost', isMonospace: true },
+    { key: 'currentPrice', header: 'Current Price', isMonospace: true },
+    { key: 'marketValue', header: 'Market Value', isMonospace: true },
+    { key: 'totalPl', header: 'Total P&L', align: 'right', isMonospace: true }
+  ];
+
+  holdings = [
+    { 
+      ticker: 'AAPL', 
+      assetName: 'Apple Inc.', 
+      quantity: '50.00', 
+      avgCost: '$172.10', 
+      currentPrice: '$187.42', 
+      marketValue: '$9,371.00', 
+      totalPl: '+$766.00' 
+    }
+  ];
+}
+
