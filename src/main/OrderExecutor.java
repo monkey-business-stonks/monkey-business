ee3266176298f736acdbb362ae951c37f5d34d33
Krrish <krrish583@gmail.com>
2026-09-24 11:05:43 -0400

    Order, OM, and OE initial setup

---
+++ src/main/OrderExecutor.java
@@ -0,0 +1,14 @@
+package main;
+
+public class OrderExecutor {
+    public OrderExecutor(Order order, Account account) {
+    }
+
+    public Order processTrade(Order order) {
+        // 1. Get Current Price
+        // 2. Get current time
+        // 3. Ping Transaction manager
+        // Idk if this needs to return an Order object
+        return order;
+    }
+}
